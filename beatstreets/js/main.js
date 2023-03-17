ba4d53f33748da8efbb88c6ef0db32b550f9a8de
skizulot16 <89729858+skizulot16@users.noreply.github.com>
2023-03-18 02:22:46 +0530

    beatstreets

---
+++ beatstreets/js/main.js
@@ -0,0 +1,410 @@
+/*  ---------------------------------------------------
+  Template Name: DJoz
+  Description:  DJoz Music HTML Template
+  Author: Colorlib
+  Author URI: https://colorlib.com
+  Version: 1.0
+  Created: Colorlib
+---------------------------------------------------------  */
+
+'use strict';
+
+(function ($) {
+
+    /*------------------
+        Preloader
+    --------------------*/
+    $(window).on('load', function () {
+        $(".loader").fadeOut();
+        $("#preloder").delay(2000).fadeOut("slow");
+    });
+
+    /*------------------
+        Background Set
+    --------------------*/
+    $('.set-bg').each(function () {
+        var bg = $(this).data('setbg');
+        $(this).css('background-image', 'url(' + bg + ')');
+    });
+
+    /*------------------
+		Navigation
+	--------------------*/
+    $(".mobile-menu").slicknav({
+        prependTo: '#mobile-menu-wrap',
+        allowParentLinks: true
+    });
+    
+    /*--------------------------
+        Event Slider
+    ----------------------------*/
+    $(".event__slider").owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 3,
+        dots: false,
+        nav: true,
+        navText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true,
+        responsive: {
+            992: {
+                items: 3,
+            },
+            768: {
+                items: 2,
+            },
+            0: {
+                items: 1,
+            },
+        }
+    });
+    
+    /*--------------------------
+        Videos Slider
+    ----------------------------*/
+    $(".videos__slider").owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 4,
+        dots: false,
+        nav: true,
+        navText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true,
+        responsive: {
+            992: {
+                items: 4,
+            },
+            768: {
+                items: 3,
+            },
+            576: {
+                items: 2,
+            },
+            0: {
+                items: 1,
+            }
+        }
+    });
+
+    /*------------------
+		Magnific
+	--------------------*/
+    $('.video-popup').magnificPopup({
+        type: 'iframe'
+    });
+
+    /*------------------
+        CountDown
+    --------------------*/
+    // For demo preview
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+
+    if(mm == 12) {
+        mm = '01';
+        yyyy = yyyy + 1;
+    } else {
+        mm = parseInt(mm) + 1;
+        mm = String(mm).padStart(2, '0');
+    }
+    var timerdate = mm + '/' + dd + '/' + yyyy;
+    // For demo preview end
+    
+
+    // Use this for real timer date
+    /* var timerdate = "2020/01/01"; */
+
+	$("#countdown-time").countdown(timerdate, function(event) {
+        $(this).html(event.strftime("<div class='countdown__item'><span>%D</span> <p>Days</p> </div>" + "<div class='countdown__item'><span>%H</span> <p>Hours</p> </div>" + "<div class='countdown__item'><span>%M</span> <p>Minutes</p> </div>" + "<div class='countdown__item'><span>%S</span> <p>Seconds</p> </div>"));
+    });
+
+    /*------------------
+		Barfiller
+	--------------------*/
+    $('#bar1').barfiller({
+        barColor: "#ffffff",
+    });
+
+    $('#bar2').barfiller({
+        barColor: "#ffffff",
+    });
+
+    $('#bar3').barfiller({
+        barColor: "#ffffff",
+    });
+
+    /*-------------------
+		Nice Scroll
+	--------------------- */
+    $(".nice-scroll").niceScroll({
+        cursorcolor: "#111111",
+        cursorwidth: "5px",
+        background: "#e1e1e1",
+        cursorborder: "",
+        autohidemode: false,
+        horizrailenabled: false
+    });
+
+})(jQuery);
+var $animation_elements = $('.animation-element');
+var $window = $(window);
+
+function check_if_in_view() {
+  var window_height = $window.height();
+  var window_top_position = $window.scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
+
+  $.each($animation_elements, function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
+
+    //check to see if this current container is within viewport
+    if ((element_bottom_position >= window_top_position) &&
+      (element_top_position <= window_bottom_position)) {
+      $element.addClass('in-view');
+    } else {
+      $element.removeClass('in-view');
+    }
+  });
+}
+
+$window.on('scroll resize', check_if_in_view);
+$window.trigger('scroll');
+
+
+var cartId = "cart";
+
+var localAdapter = {
+
+    saveCart: function (object) {
+
+        var stringified = JSON.stringify(object);
+        localStorage.setItem(cartId, stringified);
+        return true;
+
+    },
+    getCart: function () {
+
+        return JSON.parse(localStorage.getItem(cartId));
+
+    },
+    clearCart: function () {
+
+        localStorage.removeItem(cartId);
+
+    }
+
+};
+
+var ajaxAdapter = {
+
+    saveCart: function (object) {
+
+        var stringified = JSON.stringify(object);
+        // do an ajax request here
+
+    },
+    getCart: function () {
+
+        // do an ajax request -- recognize user by cookie / ip / session
+        return JSON.parse(data);
+
+    },
+    clearCart: function () {
+
+        //do an ajax request here
+
+    }
+
+};
+
+var storage = localAdapter;
+
+var helpers = {
+
+    getHtml: function (id) {
+
+        return document.getElementById(id).innerHTML;
+
+    },
+    setHtml: function (id, html) {
+
+        document.getElementById(id).innerHTML = html;
+        return true;
+
+    },
+    itemData: function (object) {
+
+        var count = object.querySelector(".count"),
+            patt = new RegExp("^[1-9]([0-9]+)?$");
+        count.value = (patt.test(count.value) === true) ? parseInt(count.value) : 1;
+
+        var item = {
+
+            name: object.getAttribute('data-name'),
+            price: object.getAttribute('data-price'),
+            id: object.getAttribute('data-id'),
+            count: count.value,
+            total: parseInt(object.getAttribute('data-price')) * parseInt(count.value)
+
+        };
+        return item;
+
+    },
+    updateView: function () {
+
+        var items = cart.getItems(),
+            template = this.getHtml('cartT'),
+            compiled = _.template(template, {
+                items: items
+            });
+        this.setHtml('cartItems', compiled);
+        this.updateTotal();
+
+    },
+    emptyView: function () {
+
+        this.setHtml('cartItems', '<p>Nothing to see here</p>');
+        this.updateTotal();
+
+    },
+    updateTotal: function () {
+
+        this.setHtml('totalPrice', '₹'+cart.total);
+
+    }
+
+};
+
+var cart = {
+
+    count: 0,
+    total: 0,
+    items: [],
+    getItems: function () {
+
+        return this.items;
+
+    },
+    setItems: function (items) {
+
+        this.items = items;
+        for (var i = 0; i < this.items.length; i++) {
+            var _item = this.items[i];
+            this.total += _item.total;
+        }
+
+    },
+    clearItems: function () {
+
+        this.items = [];
+        this.total = 0;
+        storage.clearCart();
+        helpers.emptyView();
+
+    },
+    addItem: function (item) {
+
+        if (this.containsItem(item.id) === false) {
+
+            this.items.push({
+                id: item.id,
+                name: item.name,
+                price: item.price,
+                count: item.count,
+                total: item.price * item.count
+            });
+
+            storage.saveCart(this.items);
+
+
+        } else {
+
+            this.updateItem(item);
+
+        }
+        this.total += item.price * item.count;
+        this.count += item.count;
+        helpers.updateView();
+
+    },
+    containsItem: function (id) {
+
+        if (this.items === undefined) {
+            return false;
+        }
+
+        for (var i = 0; i < this.items.length; i++) {
+
+            var _item = this.items[i];
+
+            if (id == _item.id) {
+                return true;
+            }
+
+        }
+        return false;
+
+    },
+    updateItem: function (object) {
+
+        for (var i = 0; i < this.items.length; i++) {
+
+            var _item = this.items[i];
+
+            if (object.id === _item.id) {
+
+                _item.count = parseInt(object.count) + parseInt(_item.count);
+                _item.total = parseInt(object.total) + parseInt(_item.total);
+                this.items[i] = _item;
+                storage.saveCart(this.items);
+
+            }
+
+        }
+
+    }
+
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    if (storage.getCart()) {
+
+        cart.setItems(storage.getCart());
+        helpers.updateView();
+
+    } else {
+
+        helpers.emptyView();
+
+    }
+    var products = document.querySelectorAll('.product button');
+    [].forEach.call(products, function (product) {
+
+        product.addEventListener('click', function (e) {
+
+            var item = helpers.itemData(this.parentNode);
+            cart.addItem(item);
+
+
+        });
+
+    });
+
+    document.querySelector('#clear').addEventListener('click', function (e) {
+
+        cart.clearItems();
+
+    });
+
+
+});
